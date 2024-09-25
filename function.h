f77f1febcd8fa965d95d5323a5cb4e2595124c20
OliveiraGusta <gustavo.oliver456@gmail.com>
2024-09-25 13:56:17 -0300

    FIX: Bug de Deposito compartilhado

--- function.h
+++ function.h
@@ -1,32 +1,34 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
+
 #include <stdio.h>
 
 typedef struct {
-  int id;
-  char cpf[12];
-  char password[6];
-  float balanceReal;
-  float balanceBitcoin;
-  float balanceEthereum;
-  float balanceRipple;
-
+    int id;
+    char cpf[12];
+    char password[6];
+    float balanceReal;
+    float balanceBitcoin;
+    float balanceEthereum;
+    float balanceRipple;
 } User;
 
 // Tools
-void welcome();
-void diviser();
-void menu();
+void welcome();            
+void diviser();            
+void menu(int userId); 
+void loginOrRegister(User *user);
+void listUsers();     
+      
 
-// User Login
-void loginUser();
-void registerUser(User *user);
-void loginOrRegister();
+// User
+void loginUser(User *user);            
+void registerUser(User *user);         
+void deposit(int userId);              
+void checkUserInfos();    
+
 
 // Files
-void listUsers();
-void checkUserInfos();
-void deposit();
-FILE *openFile(const char *filename, const char *mode);
+FILE *openFile(const char *filename, const char *mode); 
 
 #endif
